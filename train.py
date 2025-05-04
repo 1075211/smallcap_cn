ff58a78783c35a1137051a1e6be58801b91d241a
1075211 <98676573+1075211@users.noreply.github.com>
2025-05-04 22:08:15 +0800

    Update train.py

--- train.py
+++ train.py
@@ -26,12 +26,10 @@ def get_model_and_auxiliaries(args):
         AutoConfig.register("this_xglm", ThisXGLMConfig)
         AutoModel.register(ThisXGLMConfig, ThisXGLMForCausalLM)
         AutoModelForCausalLM.register(ThisXGLMConfig, ThisXGLMForCausalLM)
-
     elif "opt" in args.decoder_name:
         AutoConfig.register("this_opt", ThisOPTConfig)
         AutoModel.register(ThisOPTConfig, ThisOPTForCausalLM)
         AutoModelForCausalLM.register(ThisOPTConfig, ThisOPTForCausalLM)
-
     else:
         AutoConfig.register("this_gpt2", ThisGPT2Config)
         AutoModel.register(ThisGPT2Config, ThisGPT2LMHeadModel)
@@ -47,7 +45,8 @@ def get_model_and_auxiliaries(args):
     tokenizer.pad_token = PAD_TOKEN
     tokenizer.eos_token = EOS_TOKEN
 
-    model = SmallCap.from_encoder_decoder_pretrained(args.encoder_name, args.decoder_name, cross_attention_reduce_factor=cross_attention_reduce_factor)
+    model = SmallCap.from_encoder_decoder_pretrained(args.encoder_name, args.decoder_name,
+                                                     cross_attention_reduce_factor=cross_attention_reduce_factor)
     model.config.vocab_size = model.config.decoder.vocab_size
     model.config.decoder_start_token_id = None
     model.config.pad_token_id = tokenizer.pad_token_id 
@@ -58,16 +57,15 @@ def get_model_and_auxiliaries(args):
         model.config.retrieval_encoder = args.retrieval_encoder   
     model.config.max_length = CAPTION_LENGTH   
     model.config.rag = not args.disable_rag
-  
-    # freeze parameters
+
     for param in model.encoder.parameters():
         param.requires_grad = False
 
     if "xglm" in args.decoder_name or "opt" in args.decoder_name:
         if not args.train_decoder:
-                for name, param in model.decoder.named_parameters():
-                    if 'encoder_attn' not in name:
-                        param.requires_grad = False
+            for name, param in model.decoder.named_parameters():
+                if 'encoder_attn' not in name:
+                    param.requires_grad = False
     else:
         if not args.train_decoder:
             for name, param in model.decoder.named_parameters():
@@ -81,28 +79,27 @@ def get_model_and_auxiliaries(args):
     return model, tokenizer, feature_extractor
 
 def get_data(tokenizer, max_length, args):
-    # Ensure the correct paths for training and validation data
     data = load_data_for_training(args.annotations_path, args.captions_path)
     train_df = pd.DataFrame(data['train'])
 
     if args.ablation_visual:
-        train_dataset =  AblationFeaturesDataset(
-                            df=train_df,
-                            features_path=os.path.join(args.features_dir, 'train.hdf5'),
-                            tokenizer=tokenizer,
-                            rag=not args.disable_rag,
-                            template_path=args.template_path,
-                            k=args.k,
-                            max_caption_length=max_length)
+        train_dataset = AblationFeaturesDataset(
+            df=train_df,
+            features_path=os.path.join(args.features_dir, 'train.hdf5'),
+            tokenizer=tokenizer,
+            rag=not args.disable_rag,
+            template_path=args.template_path,
+            k=args.k,
+            max_caption_length=max_length)
     else:
         train_dataset = TrainDataset(
-                            df=train_df,
-                            features_path=os.path.join(args.features_dir, 'train.hdf5'),
-                            tokenizer=tokenizer,
-                            rag=not args.disable_rag,
-                            template_path=args.template_path,
-                            k=args.k,
-                            max_caption_length=max_length)
+            df=train_df,
+            features_path=os.path.join(args.features_dir, 'train.hdf5'),
+            tokenizer=tokenizer,
+            rag=not args.disable_rag,
+            template_path=args.template_path,
+            k=args.k,
+            max_caption_length=max_length)
 
     return train_dataset
 
@@ -117,7 +114,7 @@ def main(args):
         output_dir = '{}_{}M_{}'.format(model_type, args.attention_size, args.decoder_name)
 
     output_dir = os.path.join("/kaggle/working/", output_dir)
-
+    
     training_args = Seq2SeqTrainingArguments(
         num_train_epochs=args.n_epochs, 
         per_device_train_batch_size=args.batch_size, 
@@ -143,27 +140,27 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Model Training')
-    parser.add_argument("--features_dir", type=str, default="/kaggle/working/features/", help="Directory where cached input image features are stored")
-    parser.add_argument("--annotations_path", type=str, default="/kaggle/input/coco-2017-dataset/coco2017/annotations/captions_train2017.json", help="JSON file with annotations in Karpathy splits")
-    parser.add_argument("--captions_path", type=str, default="/kaggle/working/retrieved_caps_resnet50x64.json", help="JSON file with retrieved captions")
-    parser.add_argument("--experiments_dir", type=str, default="/kaggle/working/", help="Directory where trained models will be saved")
+    parser.add_argument("--features_dir", type=str, default="/kaggle/working/features/", help="Directory of image features")
+    parser.add_argument("--annotations_path", type=str, default="/kaggle/input/karpathy-splits/dataset_coco.json", help="Karpathy split JSON path")
+    parser.add_argument("--captions_path", type=str, default="/kaggle/working/retrieved_caps_resnet50x64.json", help="Retrieved captions JSON")
+    parser.add_argument("--experiments_dir", type=str, default="/kaggle/working/", help="Output directory")
 
-    parser.add_argument("--encoder_name", type=str, default="openai/clip-vit-base-patch32", help="Encoder name as found on HuggingFace or stored locally")
-    parser.add_argument("--decoder_name", type=str, default="gpt2", help="Decoder name as found on HuggingFace or stored locally")
+    parser.add_argument("--encoder_name", type=str, default="openai/clip-vit-base-patch32", help="Encoder name")
+    parser.add_argument("--decoder_name", type=str, default="gpt2", help="Decoder name")
     parser.add_argument("--attention_size", type=float, default=7, help="Cross attention parameter size")
-    parser.add_argument("--train_decoder", action="store_true", default=False, help="Whether to train the decoder in addition to the attention")
+    parser.add_argument("--train_decoder", action="store_true", default=False, help="Train the decoder")
 
-    parser.add_argument("--disable_rag", action="store_true", default=False, help="Disable retrieval augmentation")
-    parser.add_argument("--k", type=int, default=4, help="Number of retrieved captions to use in prefix")
-    parser.add_argument("--retrieval_encoder", type=str, default="RN50x64", help="Visual encoder used for retrieving captions")
-    parser.add_argument("--template_path", type=str, default="src/template.txt", help="TXT file with template")
+    parser.add_argument("--disable_rag", action="store_true", default=False, help="Disable RAG")
+    parser.add_argument("--k", type=int, default=4, help="Number of retrieved captions")
+    parser.add_argument("--retrieval_encoder", type=str, default="RN50x64", help="Retrieval encoder")
+    parser.add_argument("--template_path", type=str, default="/kaggle/working/smallcap/src/template.txt", help="Template path")
 
-    parser.add_argument("--n_epochs", type=int, default=10, help="Number of training epochs")
+    parser.add_argument("--n_epochs", type=int, default=10, help="Number of epochs")
     parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
     parser.add_argument("--batch_size", type=int, default=64, help="Batch size")
     parser.add_argument("--gradient_steps", type=int, default=1, help="Gradient accumulation steps")
 
-    parser.add_argument("--ablation_visual", action="store_true", default=False, help="Whether to blank visual features")
+    parser.add_argument("--ablation_visual", action="store_true", default=False, help="Blank visual features")
 
     args = parser.parse_args()
     main(args)
