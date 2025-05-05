80e1cbd0d875660c62077d8cfa01026b0967d056
1075211 <98676573+1075211@users.noreply.github.com>
2025-05-05 13:30:01 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,6 @@
-from .mengzi_gpt import (
-    MengziGPTConfig,
-    MengziGPTModel,
-    MengziGPTLMHeadModel
-)
+from .gpt2 import ThisGPT2Config, ThisGPT2LMHeadModel
 
-AutoConfig.register("mengzi-gpt", MengziGPTConfig)
-AutoModel.register(MengziGPTConfig, MengziGPTModel)
-AutoModelForCausalLM.register(MengziGPTConfig, MengziGPTLMHeadModel)
+# 修改注册名称以区分原版
+AutoConfig.register("mengzi-gpt", ThisGPT2Config)
+AutoModel.register(ThisGPT2Config, ThisGPT2Model)
+AutoModelForCausalLM.register(ThisGPT2Config, ThisGPT2LMHeadModel)
