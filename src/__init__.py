a1f64c4972ca9e0fae27a4bff68f254e4b4bcf3b
1075211 <98676573+1075211@users.noreply.github.com>
2025-05-05 15:56:58 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,10 @@
-from transformers import AutoConfig
-from .gpt2 import ThisGPT2Config, ThisGPT2LMHeadModel
-from .vision_encoder_decoder import SmallCapConfig
+from transformers import AutoConfig, AutoModel
+from .gpt2 import ThisGPT2Config, ThisGPT2Model, ThisGPT2LMHeadModel
+from .vision_encoder_decoder import SmallCapConfig, SmallCap
 
-# 注册自定义配置
+# 注册配置类
 AutoConfig.register("this_gpt2", ThisGPT2Config)
 AutoConfig.register("smallcap", SmallCapConfig)
+
+# 注册模型类
+AutoModel.register(ThisGPT2Config, ThisGPT2Model)
