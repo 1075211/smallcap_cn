370cbbb98aedc9e977523a15d8a11c183f507f49
1075211 <98676573+1075211@users.noreply.github.com>
2025-05-05 16:25:18 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,8 @@ from .gpt2 import ThisGPT2Config, ThisGPT2Model, ThisGPT2LMHeadModel
 from .vision_encoder_decoder import SmallCapConfig, SmallCap
 
 # 注册配置类
-AutoConfig.register("this_gpt2", ThisGPT2Config)
+# 修改注册代码（确保和 ThisGPT2Config.model_type 一致）
+AutoConfig.register("mengzi-gpt", ThisGPT2Config)  # 而不是 "this_gpt2"
 AutoConfig.register("smallcap", SmallCapConfig)
 
 # 注册模型类
